7dd5e5eaa894791a8cbe2b273c0c55ba129c70ec
zayyid123 <mochamadzayyid@gmail.com>
2024-10-06 09:25:12 +0700

    add button question

--- src/app/active/page.tsx
+++ src/app/active/page.tsx
@@ -36,7 +36,7 @@ const ActivePage = () => {
                 className={`md:w-[49%] w-[48%] h-40 ${
                   index === 0 && "bg-[#3474B1] hover:bg-[#4192dd]"
                 } ${index === 1 && "bg-[#3FA0A9] hover:bg-[#4dc1cc]"} ${
-                  index === 2 && "bg-[#F0AC31] hover:bg-[#eebc60]"
+                  index === 2 && "bg-[#F0AC31] hover:bg-[#f0c575]"
                 } ${
                   index === 3 && "bg-[#D65972] hover:bg-[#ee627e]"
                 } text-center flex justify-center items-center p-3 rounded-lg ease-in-out duration-300 cursor-pointer`}
@@ -51,20 +51,38 @@ const ActivePage = () => {
       {/* sidebar */}
       <div
         className={`fixed top-0 ${
-          isOpenQuestion ? "right-0" : "right-[200px]"
-        } w-full h-screen flex items-center ease-in-out duration-300`}
+          isOpenQuestion ? "w-full" : "w-0"
+        } h-screen flex items-center`}
       >
-        <div className="relative bg-white h-[90%] w-[200px] rounded-r-lg shadow my-auto p-5">
+        <div
+          className={`relative bg-white h-[90%] ${
+            isOpenQuestion ? "w-[300px]" : "right-10 w-0"
+          } rounded-r-lg shadow my-auto p-5`}
+        >
+          {/* question */}
+          <div className="flex w-full h-full justify-between items-center gap-x-4 flex-wrap pr-10">
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((res, index) => (
+              <div
+                key={"KotakQuestion" + index}
+                className="bg-slate-500 text-center text-white w-[60px] h-[60px] flex justify-center items-center cursor-pointer hover:bg-slate-300 ease-in-out duration-300"
+              >
+                <div>{res}</div>
+              </div>
+            ))}
+          </div>
+
           {/* btn */}
           <div
             onClick={() => {
               setisOpenQuestion(!isOpenQuestion);
             }}
-            className={`absolute ${
-              isOpenQuestion ? "-right-4" : "-right-9"
-            } bg-slate-600 text-white w-10 text-center flex justify-center items-center py-2 cursor-pointer hover:bg-slate-500 ease-in-out duration-300`}
+            className={`absolute top-5 ${
+              isOpenQuestion ? "-right-4" : "-right-10"
+            } bg-slate-800 text-white w-10 text-center flex justify-center items-center py-2 cursor-pointer hover:bg-slate-600 ease-in-out duration-300`}
           >
-            <ChevronRightIcon className="w-5 h-5" />
+            <ChevronRightIcon
+              className={`w-5 h-5 ${isOpenQuestion && "rotate-180"}`}
+            />
           </div>
         </div>
 
